ac46ac357ef582ea2de6f1e6116d85ba4c0519b7
Raghav Ramesh <raghavramesh94@gmail.com>
2014-08-20 17:51:59 +0800

    refactor(imgpros.h): rearrange methods to distinguish between CPU and GPU processing

--- imgpros.h
+++ imgpros.h
@@ -81,25 +81,26 @@ public:
     imgpros();
 
     void process_main();
-    void resizeimage(Mat src,Mat &dst,int width,int height);
     int init_camera(int width,int height);
     int process_image(Mat image);
-    int compute_skew(const Mat src);
     void deskew(Mat img, double angle,Mat &rotated);
     void display();
+    
     void display(Mat src);
     void rotate(Mat src,Mat &dst,double angle);
-    
     void callines(Mat src, Mat &dst,int thresh,unsigned int *nb,int thickness,double &Fangle,double acute);
-    void cvFillHoles(cv::Mat &input);
+    int compute_skew(const Mat src);
+    void resizeimage(Mat src,Mat &dst,int width,int height);
     bool imagesum(Mat src,int size,vector<Mat> &Tarray);
+    
+    void cvFillHoles(cv::Mat &input);
     double howwhite(Mat img);
     void FindBlobs(const cv::Mat &binary, std::vector < std::vector<cv::Point2i> > &blobs);
 
     //gpu
-    void callines(GpuMat src, GpuMat &dst,int thresh,unsigned int *nb,int thickness,double &Fangle,double acute);
-    void rotate(GpuMat src,GpuMat &dst,double angle);
     void display(GpuMat src);
+    void rotate(GpuMat src,GpuMat &dst,double angle);
+    void callines(GpuMat src, GpuMat &dst,int thresh,unsigned int *nb,int thickness,double &Fangle,double acute);
     int compute_skewg(GpuMat src);
     void resizeimage(GpuMat src,GpuMat &dst,int width,int height);
     bool imagesum(GpuMat src,int size,vector<GpuMat> &Tarray);
