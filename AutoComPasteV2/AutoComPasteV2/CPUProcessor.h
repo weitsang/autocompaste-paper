93f127acd3221915acbf4bc30a52d120945084f1
Raghav Ramesh <raghavramesh94@gmail.com>
2014-09-12 00:22:50 +0800

    feat: add methods to GPUProcessor and CPUProcessor header files

--- AutoComPasteV2/AutoComPasteV2/CPUProcessor.h
+++ AutoComPasteV2/AutoComPasteV2/CPUProcessor.h
@@ -10,5 +10,30 @@
 #define __AutoComPasteV2__CPUProcessor__
 
 #include <iostream>
+"""Displays the image in the specified window"""
+void display(Mat src);
 
+"""Applies affine transformation the src image contained in a Mat object given the angle of rotation"""
+void rotate(Mat src, Mat &dst, double angle);
+
+"""Stores frames for averaging"""
+bool storeFramesForAveraging(Mat src, int size, vector<Mat> &Tarray);
+
+"""Returns a Mat object with an image of a different resolution"""
+void resizeImage(int width, int height);	
+
+"""Calculates and draws lines with hough transform"""
+void applyHoughTransform(Mat src, Mat &dst, int threshold, unsigned int *nb, int thickness, double &Fangle, double acute);
+
+"""To calculate the skew angle of text in an image"""
+int computeSkewOfTextInImage(const Mat src);
+
+"""Finds and stores large isolated blobs within an image"""
+void findAndStoreBlobs(const cv::Mat &binary, std::vector<std::vector<cv::Point2i>> &blobs);
+
+"""Returns ratio of white content to black"""
+double getWhiteContentRatio(Mat img);
+
+"""Fills the image with to contrast lines"""
+void fillHolesWithSpecifiedColor(cv::Mat &input);
 #endif /* defined(__AutoComPasteV2__CPUProcessor__) */
--- AutoComPasteV2/AutoComPasteV2/GPUProcessor.h
+++ AutoComPasteV2/AutoComPasteV2/GPUProcessor.h
@@ -12,21 +12,21 @@
 #include <iostream>
 
 """Displays the image in the specified window"""
-virtual void displayImageInWindow(GpuMat src);
+void displayImageInWindow(GpuMat src);
 
 """Applies affine transformation the src image contained in a GpuMat object given the angle of rotation"""
-virtual void rotateClockwise(double angle);
+void rotateClockwise(double angle);
 
 """Stores frames for averaging"""
-virtual bool storeFramesForAveraging(GpuMat src, int size, vector<GpuMat> &Tarray);
+bool storeFramesForAveraging(GpuMat src, int size, vector<GpuMat> &Tarray);
 
 """Returns a GpuMat object with an image of a different resolution"""
-virtual void resizeImage(int width, int height);
+void resizeImage(int width, int height);
 
 """Calculates and draws lines with hough transform"""
-virtual void applyHoughTransform(GpuMat src, GpuMat &dst, int threshold, unsigned int *nb, int thickness, double &Fangle, double acute);
+void applyHoughTransform(GpuMat src, GpuMat &dst, int threshold, unsigned int *nb, int thickness, double &Fangle, double acute);
 
 """To calculate the skew angle of text in an image"""
-virtual int computeSkewTextInImage(GpuMat src);
+int computeSkewTextInImage(GpuMat src);
 
 #endif /* defined(__AutoComPasteV2__GPUProcessor__) */
