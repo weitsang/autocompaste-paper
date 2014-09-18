da5ba9fdc9382a7fb211fd604b5c0d4055b70307
Edwin <24edwin@gmail.com>
2014-09-19 03:00:44 +0800

    add (Page.h) : added method definitions in Page

--- AutoComPasteV2/AutoComPasteV2/Page.h
+++ AutoComPasteV2/AutoComPasteV2/Page.h
@@ -11,4 +11,30 @@
 
 #include <iostream>
 
+class Page {
+	Processor processor;
+public:
+
+	//Initiate identifiers
+	Page();
+
+	//Displays the image after rotation
+	void displayAfterRotation();
+
+	//To rotate the image to compensate for skewing
+	void deskewImage(Mat img, double angle, Mat &rotated);
+
+	//Handles phase 1 of image processing - applying morphological operations
+	int prepareImageForOCR(Mat image);
+	
+	//Set CPU/GPU
+	void setProcessor(Processor p);
+
+	//Extracts text from image and stores as a string
+	void recogniseTextUsingOCR(Mat img); 
+
+	//Returns string result of OCR
+	string OCRToString();
+}
+
 #endif /* defined(__AutoComPasteV2__Page__) */
