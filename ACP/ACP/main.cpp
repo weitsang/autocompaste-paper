1172180b3a1707a1090c0a03d27839af9231fc54
Raghav Ramesh <raghavramesh94@gmail.com>
2015-02-26 00:08:35 +0800

    fix: call required methods from main

--- ACP/ACP/main.cpp
+++ ACP/ACP/main.cpp
@@ -12,11 +12,12 @@
 #include "Page.h"
 #include "Processor.h"
 #include <OpenCL/opencl.h>
+//#include "AdvancedProcessor.cpp"
 
 int main(int argc, const char *argv[]) {
     
     Camera cam;
-    Mat image = cam.getImageFileInput("/Users/raghav/Desktop/1.jpg");
+    Mat image = cam.getImageFileInput("/Users/raghav/Desktop/4.png");
 
     // Initialise Page parameters
     Page page;
@@ -26,56 +27,18 @@ int main(int argc, const char *argv[]) {
     Processor processor;
     processor.setPage(page);
     
-    threshold(image, image, 56, 255, THRESH_BINARY);
-//    adaptiveThreshold(image, image, 255.f, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 19, 4.1f);
-    
-    
-
-//    vector<int> splittingLocations = processor.getSplittingLocations();
-//    for (int i=0; i<splittingLocations.size(); i++) {
-//        cout << splittingLocations[i] << endl;
-//        cv::line(image, cv::Point(0, splittingLocations[i]),
-//        cv::Point(image.size().width, splittingLocations[i]), cv::Scalar(0, 0, 200), 2, CV_AA);
-//    }
-    
-//    vector<Mat> testImages = processor.cutImageGivenWhiteLineLocations(splittingLocations);
-//    imshow("Cut image", testImages[0]);
-    // Dilate image - remove blemishes
-    image = processor.dilateImage(0, 0);
-    // Erode image - creates blobs in place of paragraphs
-    image = processor.erodeImage(0, 0);
-    imshow("Show", image);
-    cv::waitKey(0);
-    
-    // Find white lines
-//    processor.findWhiteLines(image);
-    
-    
-//    // Draw contours - draws boundaries around each letter, and makes the background black - less desirable
-//    processor.drawContours(0, 0);
-//    imshow("Borders", image);
-//    imwrite("/Users/raghav/Desktop/1-modified-before-dilation.jpg", image);
-    
-//    // Cutting image(Not really) - draws boundaries around characters in a line - more desirable
-//    // Read
-//    Mat img1 = imread("/Users/raghav/Desktop/2.jpg");
-//    
-//    // Detect
-//    vector<cv::Rect> letterBBoxes1 = processor.detectLetters(img1);
-//    
-//    // Display
-//    for (int i = 0; i < letterBBoxes1.size(); i++) {
-//        rectangle(img1, letterBBoxes1[i], cv::Scalar(0,255,0),3,8,0);
-//    }
-//
-//    namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-//    imshow("Contours", img1);
-    
-//    // Cut image
+    vector<int> whiteLineLocations = processor.getSplittingLocations();
+    vector<Mat> cutImages = processor.cutImageGivenWhiteLineLocations(whiteLineLocations);
+    // Cut image
 //    vector<Mat> images = processor.cutImage(230, 110);
-//    imshow("First small image", images[0]);
-
-
+    cout << "Does the vector contain anything? " << cutImages.size() << endl;
+    
+    imshow("First cut image", cutImages[3]);
+    
+    waitKey();
+    
+//    threshold(image, image, 56, 255, THRESH_BINARY);
+//    image = processor.dilateImage(0, 0);
     
     
 //    processor.prepareImageForOCR();
